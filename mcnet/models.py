a604a6ff0d7d84941180f30f99e57489b546e750
zuko3d <zuko3d@gmail.com>
2014-10-31 07:00:01 +0000

    Daily 31/10/14

--- mcnet/models.py
+++ mcnet/models.py
@@ -14,13 +14,23 @@ class MLogs(models.Model):
 
 class hmtgedition(models.Model):
 	name = models.TextField()
-	short = models.CharField(max_length=4)
+	short = models.TextField()
 	cards_total = models.IntegerField()
 	def __unicode__(self):
 		return self.name
 
 class hcard(models.Model):
 	engname = models.TextField()
+	snfd_name = models.TextField()
 	manacost = models.TextField()
+	pic = models.TextField()
+	ed_info = models.TextField()# @... edition short$card no.$rarity (one letter)@
 	def __unicode__(self):
 		return self.engname
+
+class mtm_card_edition(models.Model):
+	engname = models.TextField()
+	short = models.TextField()
+	no = models.IntegerField()
+	def __unicode__(self):
+		return self.engname + "/" + self.short
